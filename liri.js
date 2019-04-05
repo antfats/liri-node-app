14e732ca41f8630c1c3f42ff16135f203e254502
bob <antfats@hotmail.com>
2019-04-04 21:06:24 -0700

    concert-this works, as well as all responses from it

--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
+var moment = require('moment');
+moment().format();
 var userInput = process.argv[2];
 var qs = require("query-string")
-src = "https://sdk.scdn.co/spotify-player.js"
-
-
 var keys = require("./key");
 var SpotifyWebApi = require('spotify-web-api-node');
+var axios = require("axios");
+
+
+
 var spotifyApi = new SpotifyWebApi({
   clientId: 'fcecfc72172e4cd267473117a17cbd4d',
   clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
@@ -15,27 +18,30 @@ spotifyApi.setAccessToken(keys.spotify.id);
 //------------------------------------------------------------
 
 
-
+//concert this lines!!!
 if (userInput === "concert-this") {
   var artist = process.argv
   artist = artist.slice(3)
   artist = artist.join();
-  artist = artist.replace(",","%")
+  artist = artist.replace(",", "%20")
   var axios = require("axios");
-  var apikey =process.env.BANDS_ID;
-  console.log(artist);
-  axios.get("https://rest.bandsintown.com/artists/" + artist + "?app_id=99ab2666-e9aa-4536-abb6-4994b7cb5f98").then(function (response) {
-    console.log(response);
-  
-    // console.log(response.data[1].venue.name);
-    // console.log(response.data[1].datetime);
-  });
-
+  var apikey = process.env.BANDS_ID;
+  var url = ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=99ab2666-e9aa-4536-abb6-4994b7cb5f98");
+  axios.get(url).then(function (response) {
+    var date = response.data[1].datetime;
+    var dateObject = moment(date, "YYYY-MM-DD")
+    dateObject = moment(dateObject).toDate("YYYY-MM-DD")
+  var city = response.data[1].venue.city;
+  console.log(response.data[1].venue.name);
+  console.log("\nCity: " + city);
+  console.log("\nCountry: " + response.data[1].venue.country);
+  console.log("\nDate: " + (dateObject));
+});
+//-----------------------------------------------------------------------------------------------------------------
 }
 
 //movie-this!!!
 else if (userInput === "movie-this") {
-  var axios = require("axios");
   var movie = process.argv;
 
   axios.get("http://www.omdbapi.com/?t=" + (movie.slice(3)) + "&y=&plot=short&apikey=ae0bd42b").then(function (response) {
